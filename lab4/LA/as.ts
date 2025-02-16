7ff6b0d423957602bd4cba7bbcdbfb360345f300
d0n19 <d_maksot@kbtu.kz>
2025-02-16 20:34:10 +0500

    Add files via upload

---
+++ lab4/LA/as.ts
@@ -0,0 +1,9 @@
+
+
+    "angularCompilerOptions": {
+      "enableI18nLegacyMessageIdFormat": false,
+      "strictInjectionParameters": true,
+      "strictInputAccessModifiers": true,
+      "strictTemplates": true
+    }
+  
